6afa608a7e7b5b0b97d98db780718cd1eb03f799
AHMED_A_A_A <100471964+AHMED6011@users.noreply.github.com>
2023-05-24 11:40:55 +0300

    Update main.js
    Fixed statistics section

--- js/main.js
+++ js/main.js
@@ -10,36 +10,34 @@ window.onscroll = function () {
   scrollFunction();
 };
 
-window.onload = function () {
-  let vWebsite = new CountUp("website-count", 0, 250, 0, 2.5);
-  let vApp = new CountUp("app-count", 0, 100, 0, 2.5);
-  let vUser = new CountUp("user-count", 0, 10000, 0, 2.5);
-  let vVideo = new CountUp("video-count", 0, 30, 0, 2.5);
-  vWebsite.start();
-  vApp.start();
-  vUser.start();
-  vVideo.start();
-};
+let vWebsite = new CountUp("website-count", 0, 250, 0, 3.5);
+let vApp = new CountUp("app-count", 0, 100, 0, 3.5);
+let vUser = new CountUp("user-count", 0, 10000, 0, 3.5);
+let vVideo = new CountUp("video-count", 0, 30, 0, 3.5);
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 1900) {
+    vWebsite.start();
+    vApp.start();
+    vUser.start();
+    vVideo.start();
+  }
+});
 
-(() => {
-  "use strict";
+const forms = document.querySelectorAll(".needs-validation");
 
-  const forms = document.querySelectorAll(".needs-validation");
+Array.from(forms).forEach((form) => {
+  form.addEventListener(
+    "submit",
+    (event) => {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
 
-  Array.from(forms).forEach((form) => {
-    form.addEventListener(
-      "submit",
-      (event) => {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-
-        form.classList.add("was-validated");
-      },
-      false
-    );
-  });
-})();
+      form.classList.add("was-validated");
+    },
+    false
+  );
+});
 
 document.getElementById("fullYear").innerHTML = new Date().getFullYear();
